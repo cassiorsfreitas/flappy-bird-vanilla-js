c692bd01898ab264f1a3f290b52e11eab28ad54b
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2021-03-06 22:24:28 +0000

    added pack of barriers and start functions

--- js/script.js
+++ js/script.js
@@ -12,10 +12,10 @@ function Barrier(reverse = false) {
     this.element.appendChild(reverse ? body : border)
     this.element.appendChild(reverse ? border : body)
 
-    this.setHigh = high => body.style.height = `${high}px`
+    this.setHeight = height => body.style.height = `${height}px`
 }
 
-function PairOfBarriers (high, gap, x) {
+function PairOfBarriers (height, gap, x) {
     this.element = newElement('div', 'pair-of-barriers')
 
     this.upper = new Barrier(true)
@@ -25,13 +25,13 @@ function PairOfBarriers (high, gap, x) {
     this.element.appendChild(this.lower.element)
 
     this.randomGap = () => {
-        const highUpper = Math.random() * (high - gap)
-        const lowUpper = high - gap - highUpper
-        this.upper.setHigh(highUpper)
-        this.lower.setHigh(lowUpper)
+        const heightUpper = Math.random() * (height - gap)
+        const lowUpper = height - gap - heightUpper
+        this.upper.setHeight(heightUpper)
+        this.lower.setHeight(lowUpper)
     }
 
-    this.getX = () => parseInt(this.element.style.lef.split('px')[0])
+    this.getX = () => parseInt(this.element.style.left.split('px')[0])
     this.setX = x => this.element.style.left = `${x}px`
     this.getWidth = () => this.element.clientWidth
 
@@ -39,5 +39,40 @@ function PairOfBarriers (high, gap, x) {
     this.setX(x)
 }
 
-const b = new PairOfBarriers(700, 300, 400)
-document.querySelector('[wm-flappy]').appendChild(b.element)
+// const b = new PairOfBarriers(700, 300, 400)
+// document.querySelector('[wm-flappy]').appendChild(b.element)
+
+function BarriersPack(height, width, gap, distanceBetweenBarriers, notificationPoint) {
+    this.pairs = [
+        new PairOfBarriers(height, gap, width),
+        new PairOfBarriers(height, gap, width + distanceBetweenBarriers),
+        new PairOfBarriers(height, gap, width + distanceBetweenBarriers * 2),
+        new PairOfBarriers(height, gap, width + distanceBetweenBarriers * 3)
+    ]
+
+    const pixelSpeed = 3
+
+    this.start = () => {
+        this.pairs.forEach( pair => {
+            pair.setX(pair.getX() - pixelSpeed)
+
+            if(pair.getX() < -pair.getWidth()) {
+                pair.setX(pair.getX() + distanceBetweenBarriers * this.pairs.length)
+                pair.randomGap()
+            }
+
+            const middle = width / 2
+            const passedCenter = pair.getX() + pixelSpeed >= middle
+                && pair.getX() < middle
+            if (passedCenter) notificationPoint()
+        })        
+    }
+}
+
+const barriers = new BarriersPack(700, 1100, 400, 400)
+const area = document.querySelector('[wm-flappy]')
+barriers.pairs.forEach(pair => area.appendChild(pair.element))
+setInterval(() => {
+    barriers.start()
+}, 20)
+
